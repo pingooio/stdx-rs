b791d4f79cb06d067e59f897c067461f7e642126
pingoo <code@pingoo.io>
2026-06-10 06:34:17 +0000

    remove async-trait

--- async-trait/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // Warning: build.rs is not published to crates.io.
-
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-check-cfg=cfg(async_trait_nightly_testing)");
-}
